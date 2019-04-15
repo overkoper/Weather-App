3c432d103ff5e386a5e234cee903605ca9a76746
overkoper <piotr.kopczynski7@gmail.com>
2019-04-16 01:03:17 +0200

    added fetch and day genarator with day name prop

--- weather-app/src/Day/Day.js
+++ weather-app/src/Day/Day.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 class Day extends Component{
     state ={
-        
+        dayName: this.props.dayName
     }
     render(){
         const Wrapper = styled.div`
@@ -14,10 +14,16 @@ class Day extends Component{
         background-color: ${props => props.active&&"red"}
         width: ${props => props.active&&"200px"}
         `
+        const { dayName } = this.state;
         
         return(
-<Wrapper active>
-    text
+<Wrapper>
+    <div>{dayName}</div>
+    {/* <img alt="weather symbol" src={this.state.weatherImg} />
+    <div>
+        <span>{this.props.temperatureMax}</span>
+        <span>{this.props.temperatureMin}</span>
+    </div> */}
 </Wrapper>
         )
     }
--- weather-app/src/WeekView/WeekView.js
+++ weather-app/src/WeekView/WeekView.js
@@ -3,14 +3,25 @@ import styled from "styled-components";
 import Day from "../Day";
 
 class WeekView extends Component {
+  state={
+    weekId: "",
+    days: []
+  }
+  componentDidMount(){
+    fetch(process.env.PUBLIC_URL + "./weather.json").then(response=>response.json()).then(data=>Object.entries(data)).then(arr=>this.setState({weekId:arr[0][0], days:Object.entries(arr[0][1])}))
+    
+  }
   render() {
+    console.log(this.state.days)
     const Wrapper = styled.div`
+    display: flex;
       width: 1000px;
       margin: 0px 12px 0px 12px;
     `;
+    const { days } = this.state;
     return( 
     <Wrapper>
-        <Day />
+      {days.map(day=><Day dayName={day[0]}/>)}
     </Wrapper>);
   }
 }
