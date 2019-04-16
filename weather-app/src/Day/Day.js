30b60b237bf0c0dc37186b2b8b0e852a0bd59d76
overkoper <piotr.kopczynski7@gmail.com>
2019-04-16 09:41:26 +0200

    added weather pics and props display in day component

--- weather-app/src/Day/Day.js
+++ weather-app/src/Day/Day.js
@@ -1,31 +1,41 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import sunny from "../img/sunny.svg";
+import cloudy from "../img/cloudy.svg";
+import partaly from "../img/partaly-cloudy.svg";
+import rainy from "../img/rainy.svg";
 
-class Day extends Component{
-    state ={
-        dayName: this.props.dayName
-    }
-    render(){
-        const Wrapper = styled.div`
+class Day extends Component {
+  state = {
+    dayName: this.props.dayName,
+    weather: this.props.weather,
+    temp: this.props.temp
+  };
+  render() {
+    const Wrapper = styled.div`
         width: 140px;
         height: 200px;
         border: 1px solid grey;
-        position: ${props => props.active&&"absolute"}
-        background-color: ${props => props.active&&"red"}
-        width: ${props => props.active&&"200px"}
+        position: ${props => props.active && "absolute"}
+        background-color: ${props => props.active && "red"}
+        width: ${props => props.active && "200px"}
+        `;
+        const Weather = styled.img`
+        width:70%;
+        height: 70%;
         `
-        const { dayName } = this.state;
-        
-        return(
-<Wrapper>
-    <div>{dayName}</div>
-    {/* <img alt="weather symbol" src={this.state.weatherImg} />
-    <div>
-        <span>{this.props.temperatureMax}</span>
-        <span>{this.props.temperatureMin}</span>
-    </div> */}
-</Wrapper>
-        )
-    }
+    const { dayName, weather, temp } = this.state;
+    console.log(weather);
+    return (
+      <Wrapper>
+        <div>{dayName}</div>
+        <Weather alt="weather symbol" src={sunny} />
+        <div>
+          <span>Day: {temp.day}</span>
+          <span>Night: {temp.night}</span>
+        </div>
+      </Wrapper>
+    );
+  }
 }
 export default Day;
--- weather-app/src/WeekView/WeekView.js
+++ weather-app/src/WeekView/WeekView.js
@@ -3,26 +3,37 @@ import styled from "styled-components";
 import Day from "../Day";
 
 class WeekView extends Component {
-  state={
+  state = {
     weekId: "",
     days: []
-  }
-  componentDidMount(){
-    fetch(process.env.PUBLIC_URL + "./weather.json").then(response=>response.json()).then(data=>Object.entries(data)).then(arr=>this.setState({weekId:arr[0][0], days:Object.entries(arr[0][1])}))
-    
+  };
+  componentDidMount() {
+    fetch(process.env.PUBLIC_URL + "./weather.json")
+      .then(response => response.json())
+      .then(data => Object.entries(data))
+      .then(arr =>
+        this.setState({ weekId: arr[0][0], days: Object.entries(arr[0][1]) })
+      );
   }
   render() {
-    console.log(this.state.days)
     const Wrapper = styled.div`
-    display: flex;
+      display: flex;
       width: 1000px;
       margin: 0px 12px 0px 12px;
     `;
     const { days } = this.state;
-    return( 
-    <Wrapper>
-      {days.map(day=><Day dayName={day[0]}/>)}
-    </Wrapper>);
+    return (
+      <Wrapper>
+        {days.map(day => (
+          <Day
+            dayName={day[0]}
+            date={day[1].date}
+            weather={day[1].weather}
+            temp={day[1].temperature}
+          />
+        ))}
+      </Wrapper>
+    );
   }
 }
 export default WeekView;
