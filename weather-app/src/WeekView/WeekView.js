c20e3b2e943957afacec6da0976ad4553cd297ae
overkoper <piotr.kopczynski7@gmail.com>
2019-04-19 17:48:34 +0200

    added full week arrays

--- weather-app/src/WeekView/WeekView.js
+++ weather-app/src/WeekView/WeekView.js
@@ -1,51 +1,69 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import * as moment from 'moment';
+import * as moment from "moment";
 import Day from "../Day";
 
 class WeekView extends Component {
   state = {
     forecast: [],
     today: "",
-    day1Array: [],
-    day2Start: null
+    day1Array: []
   };
   componentDidMount() {
-      fetch("http://api.openweathermap.org/data/2.5/forecast?q=Gdansk,pl&units=metric&lang=pl&APPID=cc4d5fa4446c609bb569254b030bb1d7")
-      .then(response=> response.json())
-      .then(data=> this.setState({
-        forecast: data.list,
-        today: data.list[0].dt_txt.split(" ")[0],
-        day1Array: data.list.filter(item=>item.dt_txt.split(" ")[0]===data.list[0].dt_txt.split(" ")[0]),
-        day2Start: data.list.filter(item=>item.dt_txt.split(" ")[0]===data.list[0].dt_txt.split(" ")[0])[data.list.filter(item=>item.dt_txt.split(" ")[0]===data.list[0].dt_txt.split(" ")[0]).length-1].dt+10800
-      }))
+    fetch(
+      "http://api.openweathermap.org/data/2.5/forecast?q=Gdansk,pl&units=metric&lang=pl&APPID=cc4d5fa4446c609bb569254b030bb1d7"
+    )
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          forecast: data.list,
+          today: data.list[0].dt_txt.split(" ")[0],
+          day1Array: data.list.filter(day=>day.dt_txt.split(" ")[0]===data.list[0].dt_txt.split(" ")[0])
+        })
+      );
   }
   render() {
-    const day3Start = this.state.day2Start + 86400
-    const day4Start = this.state.day3Start + 86400
-    const day5Start = this.state.day4Start + 86400
-    
-    console.log(moment(this.state.day2Start).format())
-    console.log(this.state.forecast[0]&&moment(this.state.forecast[0].dt).format())
-    console.log(day4Start)
-    console.log(day5Start)
+    const { forecast, day1Array } = this.state
+
+    const day2Start =
+      day1Array[day1Array.length - 1] &&
+      day1Array[day1Array.length - 1].dt + 10800;
+    const day3Start = day2Start + 86400;
+    const day4Start = day3Start + 86400;
+    const day5Start = day4Start + 86400;
+    const day6Start = day5Start + 86400;
+
+    const day2Array = forecast.filter(
+      day => day.dt >= day2Start && day.dt < day3Start
+    );
+    const day3Array = forecast.filter(
+      day => day.dt >= day3Start && day.dt < day4Start
+    );
+    const day4Array = forecast.filter(
+      day => day.dt >= day4Start && day.dt < day5Start
+    );
+    const day5Array = forecast.filter(
+      day => day.dt >= day5Start && day.dt < day6Start
+    );
+    const day6Array = forecast.filter(day => day.dt >= day6Start);
+
     const Wrapper = styled.div`
       display: flex;
       width: 1000px;
       margin: 0px 12px 0px 12px;
     `;
-    const { day1Array } = this.state;
     return (
       <Wrapper>
-        {day1Array&&day1Array.map(day => (
-          <Day
-            key={day.dt}
-            dayName={day.dt}
-            date={day.dt_txt}
-            weather={day.weather[0]}
-            temp={day.main}
-          />
-        ))}
+        {this.state.day1Array &&
+          this.state.day1Array.map(day => (
+            <Day
+              key={day.dt}
+              dayName={day.dt}
+              date={day.dt_txt}
+              weather={day.weather[0]}
+              temp={day.main}
+            />
+          ))}
       </Wrapper>
     );
   }
