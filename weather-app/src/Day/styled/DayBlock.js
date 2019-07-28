fe664c967277b7af367abb8df4b2728f160c9df9
overkoper <piotr.kopczynski7@gmail.com>
2019-07-28 21:11:10 +0200

    add styled components in separate files

---
+++ weather-app/src/Day/styled/DayBlock.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const DayBlock = styled.div`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 160px;
+      height: 200px;
+      padding-bottom: 10px;
+    `;
---
+++ weather-app/src/Day/styled/Weather.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Weather = styled.img`
+      width: 70%;
+      height: 70%;
+    `;
